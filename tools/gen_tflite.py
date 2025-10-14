f8e61fadcf3b910eba4a0f186f3c3fdbc9f9c02d
ngcvien <vienhn.24ic@vku.udn.vn>
2025-10-14 20:23:54 +0700

    feat: cập nhật cấu trúc dự án

---
+++ tools/gen_tflite.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Load model
+model = tf.keras.models.load_model('sign_model.h5')
+
+# Convert sang TFLite
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Lưu lại
+with open('sign_model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+print("✅ Đã tạo sign_model.tflite")
